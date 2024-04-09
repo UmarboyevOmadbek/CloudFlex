9899571f36df9f73fb0427c86f1d314d5f6fbd82
Omadbek <omadbekumarboyev2000@gmail.com>
2024-04-09 20:59:16 +0500

    add Homepage

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div id="Home">
+      <div id="Text">
+        <h1>WHEN PERFORMANCE MATTERS</h1>
+        <button id="TextBtn">Start development with us!</button>
+      </div>
+    </div>
+  );
+};
