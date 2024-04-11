d90aac7277f0359f3a054469f3c31afe1345acec
Omadbek <omadbekumarboyev2000@gmail.com>
2024-04-11 17:21:46 +0500

    add header

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import "./Home.css";
 
+//img
+import BirdCookies from "../../Assets/Img/BirdCookies.png";
+
 export const Home = () => {
   return (
     <div id="Home">
-      <div id="Text">
-        <h1>WHEN PERFORMANCE MATTERS</h1>
-        <button id="TextBtn">Start development with us!</button>
+      <div id="Header">
+        <div id="Text">
+          <h1>WHEN PERFORMANCE MATTERS</h1>
+          <button id="TextBtn">Start development with us!</button>
+        </div>
+        <div id="Cookies">
+          <img src={BirdCookies} alt="BirdCookies" />
+          <div id="Notice">
+            <p>
+              WE USE COOKIES TO ENSURE YOUR BEST EXPERIENCE. THROUGH YOUR
+              CONTINUED USE OF THIS SITE YOU ACCEPT THIS USE. FOR MORE
+              INFORMATION, PLEASE SEE OUR PRIVACY POLICY.
+            </p>
+            <button>ACCEPT</button>
+          </div>
+        </div>
       </div>
     </div>
   );
