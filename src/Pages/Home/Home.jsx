b42b59418672f3155d575258bc4eaf68ea86ac1b
Omadbek <omadbekumarboyev2000@gmail.com>
2024-04-19 19:16:21 +0500

    add Cookies

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Home.css";
 
 //img
-import BirdCookies from "../../Assets/Img/BirdCookies.png";
+// import BirdCookies from "../../Assets/Img/BirdCookies.png";
 
 export const Home = () => {
   return (
@@ -13,14 +13,15 @@ export const Home = () => {
           <button id="TextBtn">Start development with us!</button>
         </div>
         <div id="Cookies">
-          <img src={BirdCookies} alt="BirdCookies" />
+          {/* <img src={BirdCookies} alt="BirdCookies" /> */}
+          <div id="imgBRD"></div>
           <div id="Notice">
             <p>
-              WE USE COOKIES TO ENSURE YOUR BEST EXPERIENCE. THROUGH YOUR
-              CONTINUED USE OF THIS SITE YOU ACCEPT THIS USE. FOR MORE
-              INFORMATION, PLEASE SEE OUR PRIVACY POLICY.
+              WE USE COOKIES TO ENSURE YOUR BEST EXPERIENCE. <br /> THROUGH YOUR
+              CONTINUED USE OF THIS SITE YOU ACCEPT THIS USE. <br />
+              FOR MORE INFORMATION, PLEASE SEE OUR PRIVACY POLICY.
             </p>
-            <button>ACCEPT</button>
+            <button id="AcceptBtnH">ACCEPT</button>
           </div>
         </div>
       </div>
